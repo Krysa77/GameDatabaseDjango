409f9078592b3429a41816245e742eeee4dff7dd
Krysa77 <118102476+Krysa77@users.noreply.github.com>
2025-05-05 12:47:33 +0200

    3

--- database/admin.py
+++ database/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Hra, Hrac, Hra_has_platforma, Hra_has_zanr, Knihovna_hrace, Platforma
 
-# Register your models here.
+admin.site.register(Hra)
+admin.site.register(Hrac)
+admin.site.register(Hra_has_platforma)
+admin.site.register(Hra_has_zanr)
+admin.site.register(Knihovna_hrace)
+admin.site.register(Platforma)
