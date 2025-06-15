b620af5c0cd1d496de9ed83e6f77505c5774556b
Krysa77 <matybezdek@gmail.com>
2025-06-15 20:01:07 +0200

    6

--- database/urls.py
+++ database/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name="index"),
     path('games/', views.game_list, name="game_list"),
     path('games/<int:game_id>/', views.game_detail, name="game_detail"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
